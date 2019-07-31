8a1e5c936c4341b19f0f63efbd023dbc8d71c83d
yuchanleeme <yuchanlee.me@gmail.com>
2019-07-31 23:37:56 +0900

    BOJ (#62)

--- BOJ/BOJ_6603_로또.cpp
+++ BOJ/BOJ_6603_로또.cpp
@@ -5,46 +5,59 @@ using namespace std;
 
 /*
 <로또>
-주요:
-1.
+주요: DFS, 백트래킹
+1. 백트래킹을 이용한 풀이
+2. dfs를 수행하면서 결과벡터에 lotto번호를 삽입하고 방문처리한다.
+  => 최대값을 전달해줌으로서 중복 출력을 막는다.
+3. dfs를 나오면서 결과벡터에 lotto번호를 빼주고 방문처리를 풀어준다.
 
 */
+int N;
+vector<int> lotto;
+vector<int> result;
+vector<int> visit;
+
+void dfs(int level, int max){
+
+  if(level == 6){
+
+    for (int i = 0; i < 6; i++) {
+      cout << result[i] << " ";
+    }
+    cout <<"\n";
+    return;
+  }
+  else{
+    for (int i = max; i < N; i++) {
+      if(visit[i] == 0){
+        result.push_back(lotto[i]);
+        visit[i] = 1;
+        dfs(level + 1, i);
+        visit[i] = 0;
+        result.pop_back();
+      }
+    }
+  }
+}
+
 
 int main() {
 
-  int k, num;
-  vector<int> lotto;
-  vector<int> possibility(6, 1);
-  vector<int> ans;
+  while (cin >> N) {
+    if(N == 0){ break;}
 
-  while (cin >> k && k != 0) {
-    for (int i = 0; i < k; i++) {
+    int num;
+    lotto.clear();
+    result.clear();
+    visit.assign(N, 0);
+
+    for (int i = 0; i < N; i++) {
       cin >> num;
       lotto.push_back(num);
-      if(i > 6){
-        possibility.push_back(0);
-      }
     }
-
-    int last_idx, fix_idx = ;
-    for (int i = 0; i < k; i++) {
-      if(possibility[i] == 1){
-        last_idx = i;
-        cout << lotto[i] << " ";
-      }
-    }
-    if(last_idx < k-1){
-      possibility[last_idx] = 0;
-      possibility[last_idx + 1] = 1;
-    }
-    else{
-
-    }
-
-
+    dfs(0, 0);
+    cout << "\n";
   }
-
-
 
   return 0;
 }
