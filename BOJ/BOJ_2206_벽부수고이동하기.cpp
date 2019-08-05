65e469b3b2b1b139f4d9228e50a8e6280d034511
yuchanleeme <yuchanlee.me@gmail.com>
2019-08-05 23:48:28 +0900

    BOJ (#67)

--- BOJ/BOJ_2206_벽부수고이동하기.cpp
+++ BOJ/BOJ_2206_벽부수고이동하기.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 #include <queue>
-#include <vector>
-#include <algorithm>
 using namespace std;
 //https://www.acmicpc.net/problem/2206
 
 /*
 <벽 부수고 이동하기>
-주요: BFS , 브루트포스
-1.
+주요: BFS
+1. 미로탐색(2178)과 비슷한 문제이다.
+2. BFS로 탐색하되 각 지점마다 벽을 뚫고 도착했는지, 그냥 도착했는지를 큐에 넣어 전달해준다.
+  (x , y : 현재 위치 , (0 or 1) : 벽을 부순지 여부) => 이 값을 큐에 넣어 전달
+  => 벽을 부시지 않고 왔다
+    a. 방문하지 않았고 벽이면 부순다. (x, y, 1)
+    b. 방문하지 않았고 벽이 없으면 계속 진행한다. (x, y, 0)
+    c. 방문했다 (continue)
+  => 벽을 부시고 왔다.
+    a. 방문하지 않았고 벽이 아니다 (x, y, 1)
+    b. 방문하지 않았지만 벽이다 (continue)
+    c. 방문했다. (continue)
+3. 2번 조건에 맞게 BFS로 탐색하고 한번 움직일때마다 cnt값을 더해준다.
+4. 탐색결과값을 출력한다.
+  => 탐색 도중 해당 좌표를 찾으면 cnt 출력
+  => 큐가 빌때까지 탐색했지만 좌표에 도착하지 못하면 -1 출력
+
+==========
+* 벽을 하나씩 부시고 모든 경우를 계산하는 방법(브루트 포스, BFS)으로 풀었더니 당연히 시간초과가 났다.
+ -> 그때 그때 벽을 부셨는지 판단하는 변수를 넣어서 계산했다.
 
 */
 
+typedef struct Loc{
 
-int N, M, cnt = 0;
+  int x;
+  int y;
+  int broken;
+
+}loc;
+
+int N, M;
 int cal_x[4] = {-1, 0 ,1 , 0}, cal_y[4] = {0, -1, 0, 1};
-int wall[1001][1001] = {0, }, base_visit[1001][1001] = {0, }, visit[1001][1001] = {0, };
-queue<pair<int, int> > que;
+int wall[1001][1001] = {0, }, visit[1001][1001][2] = {0, };
+queue<loc> que;
 
-void bfs(int x, int y){
+void bfs(int x, int y, int broken){
 
   int temp_x, temp_y;
 
@@ -27,10 +50,45 @@ void bfs(int x, int y){
     temp_y = y + cal_y[i];
 
     if(temp_x >= 0 && temp_x < N && temp_y >= 0 && temp_y < M){
-      if(visit[temp_x][temp_y] == 0 && wall[temp_x][temp_y] == 0){
-        printf("%d %d\n",temp_x,temp_y);
-        que.push(make_pair(temp_x, temp_y));
-        visit[temp_x][temp_y] = 1;
+
+      // 2번
+      if(broken == 0){
+        // 안부시고 왔을 때
+        if(visit[temp_x][temp_y][0] == 1){
+          // 방문 했을 때
+          continue;
+        }
+        else{
+          //방문 안 했을 때
+          if(wall[temp_x][temp_y] == 0){
+            // 벽 안 부수고 진행
+            loc temp = {temp_x, temp_y, 0};
+            que.push(temp);
+            visit[temp_x][temp_y][0] = 1;
+          }
+          else{
+            // 벽을 부수고 진행
+            loc temp = {temp_x, temp_y, 1};
+            que.push(temp);
+            visit[temp_x][temp_y][1] = 1;
+          }
+        }
+      }
+      else{
+        // 부시고 온거
+        if(visit[temp_x][temp_y][1] == 1){
+          // 방문했을 때
+          continue;
+        }
+        else{
+          //방문 안 했을 때
+          if(wall[temp_x][temp_y] == 0){
+            // 그대로 진행
+            loc temp = {temp_x, temp_y, 1};
+            que.push(temp);
+            visit[temp_x][temp_y][1] = 1;
+          }
+        }
       }
     }
   }
@@ -38,79 +96,40 @@ void bfs(int x, int y){
 
 int main() {
 
-  vector<pair<int, int> > broken;
-
-  freopen("1aa.txt", "r", stdin);
-
   scanf("%d %d", &N, &M);
-  int ans = N*M;
 
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
       scanf("%1d", &wall[i][j]);
-      if(wall[i][j] == 1){
-        base_visit[i][j] = 1;
-        broken.push_back(make_pair(i,j));
-      }
-      else if(wall[i][j] == 0){
-        base_visit[i][j] = 0;
-      }
     }
   }
 
-  //wall이 다 0 일때를 고려
-  if(broken.size() == 0){
-    broken.push_back(make_pair(0,0));
+  loc l = {0, 0, 0};
+  que.push(l);
+  visit[0][0][0] = 1;
+
+  int temp_size, cnt = 0;
+  bool end_game = false;
+
+  while (!que.empty()) {
+    int temp_size = que.size();
+    for (int i = 0; i < temp_size; i++) {
+      bfs(que.front().x, que.front().y, que.front().broken);
+      if(que.front().x == (N-1) && que.front().y == (M-1)){ end_game = true;} // N, M까지 갔을 때
+      que.pop();
+    }
+    cnt++;
+    if(end_game) { break;}
   }
 
-  for (int b = 0; b < broken.size(); b++) {
-
-    int temp_size;
-    bool end_game = false;
-    cnt = 0;
-    while(!que.empty()){ que.pop();}
-
-    // visit 복사
-    for (int p = 0; p < N; p++) {
-      for (int q = 0; q < M; q++) {
-        visit[p][q] = base_visit[p][q];
-      }
-    }
-
-    // 벽 하나씩 부시는 경우
-    int temp_p = broken[b].first, temp_q = broken[b].second;
-    wall[temp_p][temp_q] = 0;
-    visit[temp_p][temp_q] = 0;
-
-    que.push(make_pair(0,0));
-    visit[0][0] = 1;
-
-    while (!que.empty()) {
-      temp_size = que.size();
-      for (int i = 0; i < temp_size; i++) {
-        bfs(que.front().first, que.front().second);
-        if(que.front().first + que.front().second == N + M - 2){ end_game = true;} // N, M까지 갔을 때
-        que.pop();
-      }
-      cnt++;
-      if(end_game) { break;}
-    }
-
-    if(end_game){
-      ans = min(ans, cnt);
-    }
-    // 부신 벽 복구
-    wall[temp_p][temp_q] = 1;
-    visit[temp_p][temp_q] = 0;
-    printf("============================\n");
-  }
-
-  if(ans == N*M){
-    printf("-1\n");
+  // 4번 (게임이 정상적으로 끝났으면 출력)
+  if(end_game){
+    printf("%d\n", cnt);
   }
   else{
-    printf("%d\n", ans);
+    printf("-1\n");
   }
+
   return 0;
 
 }
