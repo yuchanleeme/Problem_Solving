fe632e225e79889d133cc4ef09e6ca92ce70add2
yuchanleeme <yuchanlee.me@gmail.com>
2019-08-13 16:08:37 +0900

    BOJ (#71)

--- BOJ/BOJ_1655_가운데를말해요.cpp
+++ BOJ/BOJ_1655_가운데를말해요.cpp
@@ -1,73 +1,50 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 //https://www.acmicpc.net/problem/1655
 
 /*
 <가운데를 말해요>
 주요: 힙
-1. 중간값을 기준으로 그 값보다 작은 값들의 집합과 큰 값들의 집합으로 나눈다.
+1. 중간값을 기준으로 그 값보다 작거나 같은 값들의 집합(이하A)과 큰 값들의 집합(이하B)으로 나눈다.
+2. A집합의 가장 큰 원소는 중간값이고 이 값을 출력할것이므로 항상 A집합의 크기는 B집합의 크기보다 같거나 1커야한다.
+3. 이제 A집합과 B집합의 top값의 크기를 비교해서 둘의 순서를 맞춰준다.
+4. 마지막에 중간값(max_heap.top())을 출력한다.
 
 */
 
 int main() {
-  freopen("1aa.txt", "r", stdin);
 
-  priority_queue<int, vector<int> , less<int> > max_heap;
+  priority_queue<int> max_heap;
   priority_queue<int, vector<int> , greater<int> > min_heap;
 
-  vector<int> value;
-  int N, mid, input;
+  int N, input;
 
-  cin >> N;
+  scanf("%d", &N);
   for (int i = 0; i < N; i++) {
-    cin >> input;
+    scanf("%d", &input);
 
+    // 2번 조건을 맞추기 위함
     if(max_heap.empty()){
-      max_heap.push(input); // 1번
-    }else if(min_heap.empty()){
-      if(max_heap.top() <= input){
-        min_heap.push(input);
-      } // 2번
-      else{
-        min_heap.push(max_heap.top());
-        max_heap.pop();
-        max_heap.push(input);
-      }
+      max_heap.push(input);
+    }
+    else if(max_heap.size() == min_heap.size()){
+      max_heap.push(input);
     }
     else{
-      if(max_heap.top() > input){
-        // 중간값 기준 작을때
-        // printf("1 : %d\n",input);
-        if(max_heap.size() > min_heap.size()){
-          min_heap.push(max_heap.top());
-          max_heap.pop();
-        }
-        max_heap.push(input);
-      }
-      else if(min_heap.top() > input && input >= max_heap.top()){
-        // 중간값
-        // printf("2 : %d\n",input);
-        if(max_heap.size() > min_heap.size()){
-          min_heap.push(input);
-        }
-        else{
-          max_heap.push(input);
-        }
-      }
-      else if(min_heap.top() <= input){
-        // 중간값 기준 클 때
-        // printf("3 : %d\n",input);
-        if(max_heap.size() > min_heap.size()){
-          min_heap.push(input);
-        }
-        else{
-          max_heap.push(input);
-        }
+      min_heap.push(input);
+    }
+
+    // 3번
+    if(!max_heap.empty() && !min_heap.empty()){
+      if(max_heap.top() >= min_heap.top()){
+        max_heap.push(min_heap.top());
+        min_heap.push(max_heap.top());
+        max_heap.pop();
+        min_heap.pop();
       }
     }
-    cout << max_heap.top()<< "\n";
+    printf("%d\n", max_heap.top());
   }
 
   return 0;
