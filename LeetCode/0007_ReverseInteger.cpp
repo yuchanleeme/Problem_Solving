e05ce0bab070f83312a9fbb0a3532cedb8050a6d
yuchanleeme <yuchanlee.me@gmail.com>
2019-08-17 16:06:08 +0900

    LeetCode (#9)

--- LeetCode/0007_ReverseInteger.cpp
+++ LeetCode/0007_ReverseInteger.cpp
@@ -1,31 +1,36 @@
 /*
-////////////////////////////////
-main : Brute Force
-1.
+https://leetcode.com/problems/reverse-integer/
 
-////////////////////////////////
+main : 구현
+1. 10으로 나눈 나머지와 현재 ans값을 더한다.
+2. x를 10으로 나눈다.
+3. ans 값에 10을 곱합값과 10으로 x를 나눈 나머지를 더한다.
+4. x가 0이 될 때까지 1~3번 반복한다.
+5. 마지막에 INT 범위를 넘어서면 0을 리턴하고 아니면 ans를 리턴한다.
+=========
+* 계산값이 오버플로우가 떠서 long long 변수를 새로 선언해서 비교했더니 AC받았다.
+
 */
 
 class Solution {
 public:
   int reverse(int x) {
 
-    int trigger, ans = 0, num = 0, idx = 0;
-    int INT_MAX = 2147483647;
-    int multiple = 1;
+    long long ans = 0, temp;
 
     while (x != 0) {
-      digit[idx] = x % 10;
+      temp = x % 10;
+      ans *= 10;
+      ans += temp
       x /= 10;
-      idx++;
     }
 
-    for (int i = idx-1; i >= 0; i--) {
-      if(ans > )
-      ans += digit[i] * multiple;
-      multiple *= 10;
+    if(ans > INT_MAX || ans < INT_MIN){
+      return 0;
+    }
+    else{
+      return ans;
     }
 
-    return ans * trigger;
   }
 };
