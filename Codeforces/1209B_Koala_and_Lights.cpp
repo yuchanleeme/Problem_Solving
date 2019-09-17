c93f28bc6dbf504416fec77edaa959551321b6f3
yuchanleeme <yuchanlee.me@gmail.com>
2019-09-17 23:59:56 +0900

    Codeforces (#3)

---
+++ Codeforces/1209B_Koala_and_Lights.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//http://codeforces.com/contest/1209/problem/B
+
+int main() {
+  int N;
+  scanf("%d", &N);
+
+  int ans = 0;
+  vector<int> lights;
+
+  for (int i = 0; i < N; i++) {
+    int temp;
+    scanf("%1d", &temp);
+    lights.push_back(temp);
+    if(temp == 1){ ans++;}
+  }
+
+  vector<int> par[N];
+  for (int i = 0; i < N; i++) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    par[i].push_back(a);
+    par[i].push_back(b);
+  }
+
+  // max : 125 => 5 + Lcm(1,2,3,4,5)
+  for (int i = 1; i <= 125; i++) {
+    int res = 0;
+    for (int j = 0; j < N; j++) {
+      if(i < par[j][1]){
+        if(lights[j] == 1){ res++;}
+        continue;
+      }
+      if((i-par[j][1]) % par[j][0] == 0){
+        // change lights
+        if(lights[j] == 0){ lights[j] = 1;}
+        else{ lights[j] = 0;}
+      }
+      if(lights[j] == 1){ res++;}
+    }
+
+    ans = max(ans, res);
+  }
+
+  printf("%d\n", ans);
+
+  return 0;
+}
